592eeca31374cfab606dda045e8977befcb271f4
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-09-05 13:43:13 -0300

    Xriação do Banco de Dados e tabelas.

---
+++ Db_sqlite.py
@@ -0,0 +1,17 @@
+#%%
+import sqlite3
+
+#%%
+conexao = sqlite3.connect('BD_SQLite')
+cursor = conexao.cursor()
+cursor.execute('CREATE TABLE fornecedor (id INT, nome VARCHAR(100), endereco VARCHAR(250));')
+conexao.commit()
+conexao.close()
+# %%
+conexao = sqlite3.connect('BD_SQLite')
+cursor = conexao.cursor()
+cursor.execute('CREATE TABLE cliente (id INT, nome VARCHAR(100), cpf VARCHAR(11), data_de_cadastro DATE, endereco VARCHAR(250));')
+conexao.commit()
+conexao.close()
+
+# %%
