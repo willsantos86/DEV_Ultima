fc954c8e3fe72764b687ef3b81086454774d0391
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-09-12 08:43:29 -0300

    Update Db_sqlite.py

--- Db_sqlite.py
+++ Db_sqlite.py
@@ -8,6 +8,8 @@ cursor.execute('CREATE TABLE fornecedor (id INT, nome VARCHAR(100), endereco VAR
 conexao.commit()
 conexao.close()
 # %%
+import sqlite3
+
 conexao = sqlite3.connect('BD_SQLite')
 cursor = conexao.cursor()
 cursor.execute('CREATE TABLE cliente (id INT, nome VARCHAR(100), cpf VARCHAR(11), data_de_cadastro DATE, endereco VARCHAR(250));')
