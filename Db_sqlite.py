8bc978b3b19426f2c2df519beba6d0ca1b436d2c
William Santos <110426773+willsantos86@users.noreply.github.com>
2022-09-19 08:54:48 -0300

    Atualização

--- Db_sqlite.py
+++ Db_sqlite.py
@@ -1,19 +1,35 @@
 #%%
+#Importando pacote.
 import sqlite3
 
 #%%
+#Criando a Tabela Fornecedor.
 conexao = sqlite3.connect('BD_SQLite')
 cursor = conexao.cursor()
 cursor.execute('CREATE TABLE fornecedor (id INT, nome VARCHAR(100), endereco VARCHAR(250));')
 conexao.commit()
 conexao.close()
 # %%
-import sqlite3
-
+#Criando a Tabela Cliente.
 conexao = sqlite3.connect('BD_SQLite')
 cursor = conexao.cursor()
-cursor.execute('CREATE TABLE cliente (id INT, nome VARCHAR(100), cpf VARCHAR(11), data_de_cadastro DATE, endereco VARCHAR(250));')
+cursor.execute('CREATE TABLE cliente (id INT NOT NULL, nome VARCHAR(100), cpf VARCHAR(250) UNIQUE, PRIMARY KEY(ID));')
 conexao.commit()
 conexao.close()
 
+# %%
+#Criando a Tabela Consulta.
+conexao = sqlite3.connect('BD_SQLite')
+cursor = conexao.cursor()
+cursor.execute('CREATE TABLE consulta (id INT, medico_crm VARCHAR(10), horario VARCHAR(20), UNIQUE (medico_crm, horario));')
+conexao.commit()
+conexao.close()
+# %%
+#Criando a Tabela Categoria.
+conexao = sqlite3.connect('BD_SQLite')
+cursor = conexao.cursor()
+cursor.execute('CREATE TABLE categoria (id INT NOT NULL, nome VARCHAR(100), PRIMARY KEY (id));')
+cursor.execute('CREATE TABLE produto (id INT NOT NULL, nome VARCHAR(100), categoria_id INT NOT NULL, PRIMARY KEY (id), FOREIGN KEY (categoria_id)REFERENCES categoria (id));')
+conexao.commit()
+conexao.close()
 # %%
